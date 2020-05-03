6e039e8c2806017bfa304f70898c2c503b1929c9
mario_arriaga <Mario_Arriaga@epam.com>
2020-05-03 10:25:40 -0500

    Refactored Rooter.SquareRoot method's result calculation as per Refactor the code under test walkthrough section

--- MyMath/Rooter.cs
+++ MyMath/Rooter.cs
@@ -15,7 +15,8 @@ namespace MyMath
             while (Math.Abs(previousResult - result) > result / 1000)
             {
                 previousResult = result;
-                result = result - (result * result - input) / (2 * result);
+                result = (result + input / result) / 2;
+                //was: result = result - (result * result - input) / (2 * result);
             }
 
             return result;
